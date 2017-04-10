d4357d597cc29c58987060ef839074c1e84162ed
Alvaro Ludolf Ribeiro <alvaroludolf@gmail.com>
2017-04-09 23:06:53 -0300

    no message

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevel = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        if (autoLoadNextLevel > 0) {
+            Invoke("LoadNextLevel", autoLoadNextLevel);
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
---
+++ Assets/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusic;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(gameObject);
+        audioSource = gameObject.GetComponent<AudioSource>();
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnDestroy() {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode) {
+        int level = scene.buildIndex;
+        if (level < levelMusic.Length) {
+            AudioClip clip = levelMusic[level];
+            if (clip) {
+                audioSource.Stop();
+                audioSource.clip = clip;
+                audioSource.Play();
+            }
+        }
+    }
+
+}
